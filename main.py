491a6b96e375f56eb52161528b1c41eef5d4eeec
Pranav-Patel-123 <141993423+Pranav-Patel-123@users.noreply.github.com>
2025-07-11 16:25:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ ALLOWED_ORIGINS = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type"],
